3330a444fbc6b87fd63af0675b605dbe23cca4a7
chenlanlan1998 <53386528+chenlanlan1998@users.noreply.github.com>
2019-07-29 09:13:13 +0800

    Add files via upload

--- Assignment2.R
+++ Assignment2.R
@@ -23,3 +23,38 @@ cachemean <- function(x, ...) {
   x$setmean(m)
   m
 }
+
+makeCache <- function(m = matrix()) {
+  cm <- NULL
+  cinv <- NULL
+# getting matrix  
+  set <- function()
+    cm <<- m
+  get <- function()
+    cm
+  
+# caching the inverse of the matrix  
+  setinv <- function(cinv)
+    cinv <<- solve(cm)
+  
+  getinv <- functin()
+  cinv
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+cachesolve <- function(x,...) {
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    print("getting cached data") 
+    return(inv)
+
+  }
+  else{
+    a$set()
+    m <- x$get()
+    inv <- solve(m)
+    x$getinv(inv)
+    x$getinv()
+  }
+}
